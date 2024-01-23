e125969dd84d425871613c4c7342b5debdea7339
sudalaimani <sudalaimmanis@gmail.com>
2024-01-23 18:33:18 +0530

    test commit

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "aws_route_table_association" "private_subnet_2_association" {
   subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_route_table.id
 }
-
+#create elastic ip
 # create EIP
 resource "aws_eip" "my_nat_gateway" {
  
